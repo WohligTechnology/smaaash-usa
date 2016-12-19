42bf904606aa250390daf5db66d549abe864a4e8
Ruchira Padave <ruchiwohlig@gmail.com>
2016-12-19 19:05:46 +0530

    done

--- js/controllers.js
+++ js/controllers.js
@@ -277,22 +277,22 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     $scope.buyOnline = [{
         img: 'img/karting/1.jpg',
         text: ' Minnesota Lynx',
-        partners:'Proud Partners',
+        partners:'Official Hangout ',
         id: '583420a69f3f2cd049f2cb0e'
     }, {
         img: 'img/karting/2.jpg',
         text: ' Minnesota Vikings',
-         partners:'Proud Partners',
+         partners:'Proud Partner',
         id: '583421099f3f2cd049f2cb28'
     }, {
         img: 'img/karting/3.jpg',
         text: ' Minnesota Timberwolves',
-         partners:'Proud Partners',
+         partners:'Official Hangout ',
         id: '5847a24762aaa336cbd301bd'
     }, {
         img: 'img/karting/4.jpg',
         text: ' Minnesota Wild',
-         partners:'Proud Partners',
+         partners:'Official Hangout ',
         id: '5847a43562aaa336cbd3027a'
     }];
 
