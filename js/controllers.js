349123b7bc82b5c5e4110f8c5b10f31ac7b05e1f
sukshawohlig <sukshawohlig@gmail.com>
2017-01-06 17:29:14 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -1787,10 +1787,10 @@ $scope.title ='title to give';
     $scope.menutitle = NavigationService.makeactive("Host Party");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
-
+TemplateService.removeLoaderOn(2);
     // $scope.myUrl = window.location.href;
     $scope.myUrl = $location.absUrl();
-    TemplateService.removeLoaderOn(2);
+
     console.log("$scope.myUrl0", $scope.myUrl);
     //  $scope.$on('$viewContentLoaded', function(event) {
     //   $timeout(function() {
@@ -1848,14 +1848,17 @@ $scope.title ='title to give';
     }
 
     NavigationService.getSingleExploreSmaaash($stateParams.id, function(data) {
-        $scope.SingleHostParty1 = data.data;
-        $scope.SingleHostParty = _.chunk(data.data, 3);
-        $scope.content = _.groupBy($scope.SingleHostParty, 'hostAPartyType');
-        $scope.birthday = $scope.content['57d6a09dbd5eb9846074b419'];
-        $scope.kittyparties = $scope.content['57e1429c3da62fae1dfc560c'];
-        $scope.wedding = $scope.content['57d6a027bd5eb9846074b418'];
-        $scope.corporate = $scope.content['57e142483da62fae1dfc55f2'];
-        TemplateService.removeLoader();
+        if (data.value) {
+          $scope.SingleHostParty1 = data.data;
+          $scope.SingleHostParty = _.chunk(data.data, 3);
+          $scope.content = _.groupBy($scope.SingleHostParty, 'hostAPartyType');
+          $scope.birthday = $scope.content['57d6a09dbd5eb9846074b419'];
+          $scope.kittyparties = $scope.content['57e1429c3da62fae1dfc560c'];
+          $scope.wedding = $scope.content['57d6a027bd5eb9846074b418'];
+          $scope.corporate = $scope.content['57e142483da62fae1dfc55f2'];
+            TemplateService.removeLoader();
+        }
+
     });
 
 
@@ -1873,12 +1876,15 @@ $scope.title ='title to give';
         }).description;
     };
     NavigationService.getAllHostPartySlider(function(data) {
-        var i = 1;
-        $scope.hostPartySlider = _.each(data.data, function(key) {
-            key.order = i;
-            i++;
-        });
-        TemplateService.removeLoader();
+      if (data.value) {
+          var i = 1;
+          $scope.hostPartySlider = _.each(data.data, function(key) {
+              key.order = i;
+              i++;
+          });
+          TemplateService.removeLoader();
+
+        }
 
     })
 
@@ -2444,7 +2450,7 @@ $scope.title ='title to give';
         });
     })
 
-.controller('DrinkCtrl', function($scope, TemplateService, NavigationService, $timeout, $stateParams, $uibModal, $location) {
+.controller('DrinkCtrl', function($scope, TemplateService, NavigationService, $timeout, $stateParams, $uibModal, $location,$state ) {
     //Used to name the .html file
     $scope.template = TemplateService.changecontent("drink-party");
     $scope.menutitle = NavigationService.makeactive("Drink Party");
@@ -2470,14 +2476,14 @@ $scope.title ='title to give';
     });
 
 
-    // $scope.readMore = function(id) {
-    //     console.log("id", id);
-    //     $scope.moreDesc[id] = ($scope.moreDesc[id] == true) ? false : true;
-    //     console.log("$scope.drinkParty", $scope.drinkParty);
-    //     $scope.myDesc = _.find($scope.drinkParty1, function(n) {
-    //         return n._id == id;
-    //     }).description;
-    // };
+
+    $scope.gotFunction=function(id){
+      if (id === '57e13248d32ecf3237dcf36a') {
+        $state.go( 'host-party', {
+          id: '57bc4b10eb9c91f1025a3b54'
+        });
+      }
+    }
     $scope.readMore = function(id) {
         _.each($scope.moreDesc, function(value, property) {
             if (id != property) {
